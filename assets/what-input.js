e7404045e897df0cd0526768d30195935067b6b2
bvalgas <b.valgas@gmail.com>
2017-06-26 22:26:28 -0300

    Inicial
    Inicial

---
+++ assets/what-input.js
@@ -0,0 +1,292 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+<meta charset="utf-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Foundation | The most advanced responsive front-end framework in the world.</title>
+<link rel="icon" href="assets/img/icons/foundation-favicon.ico" type="image/x-icon">
+<link rel="stylesheet" href="/assets/css/app.css?hash=0d89befe7cf1cbf8709074822b473990">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/foundicons/3.0.0/foundation-icons.css" rel="stylesheet">
+<script src="//js.honeybadger.io/v0.3/honeybadger.min.js?hash=93598a78edfbc0208946a7de553a3382" type="text/javascript"></script>
+<script>
+      Honeybadger.configure({
+        api_key: '335fb598'
+      });
+    </script>
+<script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-2195009-2', 'auto');
+      ga('send', 'pageview');
+
+      ga('create', 'UA-2195009-27', 'auto', {name: "foundation"});
+      ga('foundation.send', 'pageview');
+
+    </script>
+</head>
+<body>
+ 
+ 
+ 
+ 
+<div class="off-canvas-wrapper">
+<div class="off-canvas-wrapper-inner" data-off-canvas-wrapper>
+<div class="off-canvas position-left" id="offCanvas" data-off-canvas data-position="left">
+<ul class="off-canvas-list">
+<li><label class="first">Foundation</label></li>
+<li><a href="index.html">Home</a></li>
+</ul>
+<hr>
+<ul class="off-canvas-list">
+<li><a href="http://zurb.com/responsive">Foundation Showcase</a></li>
+<li><a href="showcase/brands.html">Brands</a></li>
+<li><a href="showcase/case-studies.html">Case Studies</a></li>
+<li><a href="showcase/blog.html">Blog</a></li>
+<li><a href="showcase/buzz.html">Buzz</a></li>
+<li><a href="showcase/about.html">About Foundation</a></li>
+</ul>
+<hr>
+<ul class="off-canvas-list">
+<li><label class="first">Foundation for Sites</label></li>
+<li class="nav-main-item"><a class="" href="sites.html">Home</a></li>
+<li><a href="sites/getting-started.html" class="is-active">Getting Started</a></li>
+<li><a href="sites/download.html">Download</a></li>
+<li><a href="templates.html">HTML Templates</a></li>
+<li><a href="http://foundation.zurb.com/building-blocks/">Building Blocks</a></li>
+<li><a href="sites/resources.html">Resources</a></li>
+<li><a href="http://foundation.zurb.com/sites/docs/" target="_blank">Docs</a></li>
+</ul>
+<hr>
+<ul class="off-canvas-list">
+<li><label class="first">Foundation for Emails</label></li>
+<li class="nav-main-item"><a class="spaced-list-item" href="emails.html">Home</a></li>
+<li><a href="emails/getting-started.html">Getting Started</a></li>
+<li><a href="emails/download.html">Download</a></li>
+<li><a href="emails/inliner-v2.html">Inliner</a></li>
+<li><a href="emails/email-templates.html">Email Templates</a></li>
+<li><a href="emails/resources.html">Resources</a></li>
+<li><a href="emails/docs" target="_blank">Docs</a></li>
+</ul>
+<hr>
+<ul class="off-canvas-list">
+<li><label class="first">Develop</label></li>
+<li><a href="templates.html">HTML Templates</a></li>
+<li><a href="sites/resources.html">Resources</a></li>
+<li><a href="http://foundation.zurb.com/building-blocks/">Building Blocks</a></li>
+<li><a href="develop/contribute.html">Contribute</a></li>
+<li><a href="upload.html">Annotate Code</a></li>
+</ul>
+<hr>
+<ul class="off-canvas-list">
+<li><label class="first">Tutorials</label></li>
+<li><a href="learn/tutorials.html">Tutorials</a></li>
+<li><a href="http://zurb.com/university/lessons">Lessons</a></li>
+<li><a href="http://zurb.com/university/courses">Courses</a></li>
+<li><a href="http://zurb.com/university/training">Custom Training</a></li>
+<li><a href="learn/certification.html">Certification</a></li>
+<li><a href="learn/responsive-reading.html">Responsive Reading</a></li>
+</ul>
+<hr>
+<ul class="off-canvas-list">
+<li><label class="first">Get Involved</label></li>
+<li><a href="get-involved/support.html">Connect</a>
+</li>
+<li><a href="get-involved/premium-support.html">PLZ Halp</a></li>
+<li><a href="http://foundation.zurb.com/forum/">Foundation Forum</a></li>
+<li><a href="learn/events.html">Events</a></li>
+<li><a href="get-involved/faq.html">FAQs</a></li>
+<li><a href="get-involved/contribute.html">Contribute</a></li>
+<li><a href="get-involved/yetinauts.html">Yetinauts</a></li>
+<li><a href="get-involved/partners.html">Partners</a></li>
+<li><a href="/email-sponsorships.html">Sponsorships</a></li>
+</ul>
+<hr>
+<ul class="off-canvas-list">
+<li><label class="first">More ZURB Goodness</label></li>
+<li><a href="http://zurb.com/manifesto">Manifesto</a></li>
+<li><a href="http://zurb.com/services/our-work">Our Work</a></li>
+<li><a href="http://zurb.com/university/courses">Product Design Courses</a></li>
+</ul>
+<hr>
+<div class="zurb-links">
+<ul class="top">
+<li class="logo"><a href="http://zurb.com"><img src="assets/img/icons/footer-zurb-icon.png" alt="zurb"></a></li>
+<li><a href="http://zurb.com/about">About</a></li>
+<li><a href="http://zurb.com/blog">Blog</a></li>
+<li><a href="http://zurb.com/contact">Contact</a></li>
+</ul>
+</div>
+</div>
+<div class="off-canvas-content" data-off-canvas-content>
+<div class="title-bar hide-for-medium">
+<div class="title-bar-left">
+<button class="menu-icon-style" type="button" data-open="offCanvas"><img class="hamburger-icon" alt="Menu icon" src="assets/img/icons/menu-grey.png">
+<a class="title-bar-home-link" href="" target="_blank"><span class="title-bar-title"><img src="assets/img/zurb-logo.svg" alt="ZURB"><span class="title-bar-title-text">Foundation</span></a>
+</button>
+</div>
+</div>
+<nav class="marketing-topbar show-for-medium">
+<ul class="menu">
+<li class="topbar-title"><a class="zurb-logo" href="index.html">ZURB <strong>Foundation</strong></a>
+</ul>
+<ul class="dropdown menu" data-dropdown-menu data-click-open="false">
+<li class="has-submenu">
+<a href="http://zurb.com/responsive">Showcase</a>
+<ul class="submenu menu vertical" data-submenu>
+<li><a href="http://zurb.com/responsive">Foundation Showcase</a></li>
+<li><a href="showcase/brands.html">Brands</a></li>
+<li><a href="showcase/case-studies.html">Case Studies</a></li>
+<li><a href="showcase/blog.html">Blog</a></li>
+<li><a href="showcase/buzz.html">Buzz</a></li>
+<li><a href="showcase/about.html">About Foundation</a></li>
+</ul>
+</li>
+<li class="has-submenu">
+<a href="develop/getting-started.html">Develop</a>
+<ul class="submenu menu vertical" data-submenu>
+<li class="title">Frameworks</li>
+<li><a href="sites.html">Foundation for Sites</a></li>
+<li><a href="emails.html">Foundation for Email</a></li>
+<li class="divider"></li>
+<li class="title">Develop</li>
+<li><a href="templates.html">HTML Templates</a></li>
+<li><a href="sites/resources.html">Resources</a></li>
+<li><a href="http://foundation.zurb.com/building-blocks/">Building Blocks</a></li>
+<li><a href="upload.html">Annotate Code</a></li>
+</ul>
+</li>
+<li class="has-submenu">
+<a href="learn/tutorials.html">Tutorials</a>
+<ul class="submenu menu vertical" data-submenu>
+<li><a href="learn/tutorials.html">Tutorials</a></li>
+<li><a href="http://zurb.com/university/lessons">Lessons</a></li>
+<li><a href="http://zurb.com/university/courses">Courses</a></li>
+<li><a href="http://zurb.com/university/training">Custom Training</a></li>
+<li><a href="learn/certification.html">Certification</a></li>
+<li><a href="learn/responsive-reading.html">Responsive Reading</a></li>
+</ul>
+</li>
+<li class="has-submenu">
+<a href="get-involved/support.html">Get Involved</a>
+<ul class="submenu menu vertical" data-submenu>
+<li><a href="get-involved/support.html">Connect</a></li>
+<li><a href="get-involved/premium-support.html">PLZ Halp</a></li>
+<li><a href="http://foundation.zurb.com/forum/">Foundation Forum</a></li>
+<li><a href="learn/events.html">Events</a></li>
+<li><a href="get-involved/faq.html">FAQs</a></li>
+<li><a href="get-involved/contribute.html">Contribute</a></li>
+<li><a href="get-involved/yetinauts.html">Yetinauts</a></li>
+<li><a href="get-involved/partners.html">Partners</a></li>
+<li><a href="email-sponsorships.html">Sponsorships</a></li>
+</ul>
+</li>
+<li class="has-submenu">
+<a href="frameworks-docs.html">Docs</a>
+<ul class="submenu menu vertical" data-submenu>
+<li><a target="_blank" href="http://foundation.zurb.com/sites/docs/">Sites Docs</a></li>
+<li><a target="_blank" href="http://foundation.zurb.com/emails/docs">Emails Docs</a></li>
+</ul>
+</li>
+<li><a href="develop/getting-started.html" class="button">Getting Started</a></li>
+</ul>
+</nav>
+<section id="oops">
+<div class="row">
+<div class="large-9 medium-9 small-12 columns small-centered">
+<h5>404: Page Not Found</h5>
+<h1 class="oversized">Keep on looking....</h1>
+<p class="lead">Double check the URL or head back to the <a href="http://foundation.zurb.com/">homepage.</a> If you continue to get this page, email us at <a href="mailto:foundation@zurb.com">foundation@zurb.com.</a></p>
+<br>
+<h3>Or try one of these:</h3>
+<ul>
+<li><a href="http://foundation.zurb.com/sites.html">What's new in Foundation 6</a></li>
+<li><a href="http://foundation.zurb.com/learn/classes.html">Foundation Training</a></li>
+<li><a href="http://foundation.zurb.com/templates.html">HTML Templates for Foundation</a></li>
+<li><a href="http://foundation.zurb.com/support/support.html">Foundation Support</a></li>
+<li><a href="http://foundation.zurb.com/sites/getting-started.html">Get Started with Foundation 6</a></li>
+</ul>
+</div>
+</div>
+</section>
+<div class="newsletter-signup layout-404">
+<div class="row column medium-9 medium-columns">
+<div class="row">
+<div class="medium-7 large-8 columns">
+<a href="learn/responsive-reading.html">
+<h5 class="hide-for-medium-only">Download ZURB’s 17 Responsive Trends That Will Shape 2017</h5>
+<p>Stay informed with amazing responsive trends each month from ZURB.</p>
+</a>
+</div>
+<div class="medium-5 large-4 footer-signup-form columns">
+<a style="margin-bottom: 0;" href="learn/responsive-reading.html" class="button expanded">Download the PDF</a>
+</div>
+</div>
+</div>
+</div>
+<div class="zurb-footer-top bg-fblue layout-404">
+<div class="row property">
+<div class="medium-4 columns">
+<div class="property-info">
+</div>
+</div>
+<div class="medium-8 columns">
+<div class="row collapse">
+<div class="medium-4 columns">
+<div class="learn-links">
+<h4 class="hide-for-small">Want more?</h4>
+<ul>
+<li><a href="http://zurb.com/university/courses">Foundation Training</a></li>
+<li><a href="http://zurb.com/library">Design Resources</a></li>
+</ul>
+</div>
+</div>
+<div class="medium-4 columns">
+<div class="support-links">
+<h4 class="hide-for-small">Talk to us</h4>
+<p>Tweet us at <br> <a href="https://twitter.com/zurbfoundation">@ZURBfoundation</a></p>
+<p><a href="get-involved/contribute.html">Get involved in<br>our amazing community.</a></p>
+</div>
+</div>
+<div class="medium-4 columns">
+<div class="connect-links">
+<h4 class="hide-for-small">Stay Updated</h4>
+<p>Keep up with the latest on Foundation. Find us on <a href="https://github.com/zurb/foundation">Github</a>.</p>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="zurb-footer-bottom">
+<div class="row">
+<div class="medium-4 medium-push-8 columns">
+<ul class="home-social">
+<li><a href="https://www.youtube.com/ZURB" class="youtube"></a></li>
+<li><a href="https://www.twitter.com/ZURBfoundation" class="twitter"></a></li>
+<li><a href="https://www.facebook.com/ZURB" class="facebook"></a></li>
+<li><a href="http://zurb.com/contact" class="mail"></a></li>
+</ul>
+</div>
+<div class="medium-8 medium-pull-4 columns">
+<a href="http://www.zurb.com" class="zurb-logo regular"></a>
+<ul class="zurb-links">
+<li><a href="http://zurb.com/about">About</a></li>
+<li><a href="http://zurb.com/blog">Blog</a></li>
+<li><a href="http://zurb.com/news">News<span class="show-for-medium-up"> &amp; Events</span></a></li>
+<li><a href="http://zurb.com/contact">Contact</a></li>
+<li><a href="http://zurb.com/sitemap">Sitemap</a></li>
+</ul>
+<p class="copyright">&copy; 1998&dash;2017 ZURB, Inc. All rights reserved.</p>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<script src="/assets/js/templates.js?hash=280221604ca00eef359af408cc7691c9"></script>
+<script src="/assets/js/app.js?hash=e041f801d0f5d3cd535813f4326589ce"></script>
+</body>
+</html>
